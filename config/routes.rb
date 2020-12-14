403b56c87f42846d91262afd956f5f84576ee617
Victoria Gonzales <vagp89@gmail.com>
2020-12-14 22:55:28 +0100

    renamed shortcode redirect route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :urls, only: [:index, :new, :create, :show]
-  get '/s/:code', to: 'urls#redirect', as: :short
+  get '/:code', to: 'urls#redirect', as: :short
 end
