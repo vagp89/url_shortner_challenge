f1721bc3ad8a3f9121784e18cae1137b4ff8a197
Victoria Gonzales <vagp89@gmail.com>
2020-12-15 22:14:21 +0100

    last changes in the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'urls#index'
   resources :urls, only: [:index, :new, :create, :show]
   get '/:code', to: 'urls#redirect', as: :short
   get '/:code/stats', to: 'urls#stats', as: :stats
